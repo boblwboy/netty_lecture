2734bef7d84a621523e3d660c3c6172a315c7bc4
bob <boblwboy@gmail.com>
2017-10-13 11:23:59 +0800

    capacity

--- src/main/java/com/founder/nio/NioTest1.java
+++ src/main/java/com/founder/nio/NioTest1.java
@@ -7,6 +7,9 @@ public class NioTest1 {
 
     public static void main(String[] args) {
         IntBuffer buffer = IntBuffer.allocate(10);
+
+        System.out.println("capacity:"+buffer.capacity());
+
         for (int i = 0; i<buffer.capacity();i++) {
 
             int randomNumber = new SecureRandom().nextInt(20);
