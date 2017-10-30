70b4f4f93288a936b75b713496c4f51b99228cf0
bob <boblwboy@gmail.com>
2017-10-30 13:59:25 +0800

    oldio

---
+++ src/main/java/com/founder/zerocopy/OldIOClient.java
@@ -0,0 +1,38 @@
+package com.founder.zerocopy;
+
+import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
+
+import java.io.*;
+import java.net.Socket;
+
+public class OldIOClient {
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost",8899);
+
+        String fileName = "/Users/bob/Downloads/movie/Gantz.O.2016.1080p.BluRay.DTS.x264-HDS/Gantz.O.2016.1080p.BluRay.DTS.x264-HDS.mkv";
+
+        InputStream inputStream = new FileInputStream(fileName);
+
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        byte[] buffer = new byte[4096];
+
+        long readCount;
+
+        long total = 0;
+
+        long startTime = System.currentTimeMillis();
+
+        while ((readCount = inputStream.read(buffer)) >= 0) {
+            total += readCount;
+            dataOutputStream.write(buffer);
+        }
+
+        System.out.println("发送总字节数： " + total + ",耗时： " + (System.currentTimeMillis() - startTime));
+
+        dataOutputStream.close();
+        socket.close();
+        inputStream.close();
+    }
+}
---
+++ src/main/java/com/founder/zerocopy/OldIOServer.java
@@ -0,0 +1,35 @@
+package com.founder.zerocopy;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class OldIOServer {
+
+    public static void main(String[] args) throws Exception {
+
+        ServerSocket serverSocket = new ServerSocket(8899);
+
+        while (true) {
+
+            Socket socket = serverSocket.accept();
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+            try {
+                byte[] bytes = new byte[4096];
+
+                while (true) {
+                    int read = dataInputStream.read(bytes, 0, bytes.length);
+                    if (-1 == read) {
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
