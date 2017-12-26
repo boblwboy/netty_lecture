7934e6453a693f3a702adbf1f6c6d74d6e9a3ddb
bob <boblwboy@gmail.com>
2017-12-26 17:38:22 +0800

    tcp 粘包拆包 自定义协议

---
+++ src/main/java/com/founder/netty/handler3/MyClientInitializer.java
@@ -0,0 +1,19 @@
+package com.founder.netty.handler3;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * Created by liuwei on 2017-5-31.
+ */
+public class MyClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new MyPersonDecoder());
+        pipeline.addLast(new MyPersonEncoder());
+        pipeline.addLast(new MyClientHandler());
+    }
+}
---
+++ src/main/java/com/founder/netty/handler3/MyPersonEncoder.java
@@ -0,0 +1,20 @@
+package com.founder.netty.handler3;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.ReplayingDecoder;
+
+import java.util.List;
+
+public class MyPersonEncoder extends MessageToByteEncoder<PersonProtocol> {
+
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, PersonProtocol msg, ByteBuf out) throws Exception {
+        System.out.println("MyPersonEncoder encode invoked!");
+
+        out.writeInt(msg.getLength());
+        out.writeBytes(msg.getContent());
+    }
+}
